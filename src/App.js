0e9c12120f390c4fc2e31a4bef29e234dc4e6755
Bob Hunt <bob.hunt.email@gmail.com>
2018-07-31 17:46:06 -0400

    added name

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class Text extends Component {
+  render(){
+    return (
+      <div>Hello, <span style={{color: this.props.color}}>{this.props.value}</span></div>
+    );
+  }
+}
+class Text2 extends Component {
   render() {
-    console.log(this.props)
     return (
     <div>This is the color <span style={{color: this.props.color}}>{this.props.value}</span>.</div>
     );
   }
 }
+
+
 
 class App extends Component {
   render() {
@@ -19,11 +27,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to BOB's React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h2 className="App-text"><Text color="blue" value="BLUE"/></h2>
-        <h2 className="App-text"><Text color="red" value="RED"/></h2>
+        <h1 className="App-title"><Text color="Red" value="Bob"/></h1>
+
+        <h2 className="App-text"><Text2 color="blue" value="BLUE"/></h2>
+        <h2 className="App-text"><Text2 color="red" value="RED"/></h2>
+
       </div>
     );
   }
